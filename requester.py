1fe6b4e617192d499ebd4872ff8a02e8556c53b0
Ben Wurster <wursterben17@gmail.com>
2023-10-06 17:26:31 -0500

    basically done

--- requester.py
+++ requester.py
@@ -8,26 +8,26 @@ import time
 socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 this_host = socket.gethostname()
 this_ip_addr = socket.gethostbyname(this_host)
-socket.bind((this_ip_addr, port))
 
-def send_data(port, file_option, tracker_lines):
+def send_data(line):
+    ip_addr = socket.gethostbyname(line[2])
+    sender_addr = (ip_addr, int(line[3]))
 
-    for line in tracker_lines:
-        if line[0] == file_option:
-            ip_addr = socket.gethostbyname(line[2])
-            sender_addr = (ip_addr, line[3])
+    # create udp header (R since its a request packet)
+    udp_header = struct.pack("!cII", b'R', 0, 0)
+    # combine header with payload
+    packet_with_header = udp_header + line[0].encode()
 
-            # create udp header (R since its a request packet)
-            udp_header = struct.pack("!cII", 'R', 0, 0)
-            # combine header with payload
-            packet_with_header = udp_header + file_option
-
-            # send this request to the sender
-            socket_obj.sendto(packet_with_header, sender_addr)
+    # send this request to the sender
+    socket_obj.sendto(packet_with_header, sender_addr)
 
 def receive_data():
     # decide when this ends...
     start_time = time.time()
+
+    total_packets = 0
+    total_bytes = 0
+
     while True:
         full_packet, (sender_ip, sender_port) = socket_obj.recvfrom(1024)
         udp_header = full_packet[:9]
@@ -35,44 +35,39 @@ def receive_data():
         udp_header = struct.unpack("!cII", udp_header)
 
         packet_type = {}
-        packet_type['D'] = "DATA"
-        packet_type['E'] = "END"
-
-        total_packets = 0
-        total_bytes = 0
+        packet_type[b'D'] = "DATA"
+        packet_type[b'E'] = "END"
 
         # process packet with print statements
         print("Requester's print information")
 
-        print(packet_type[udp_header[0]] + " Packet\n")
-        print("recv time   " + str(datetime.now()) + "\n")
-        print("Sender ip address:  " + sender_ip + "\n")
-        print("Sender port:  " + sender_port + "\n")
-        print("sequence:  " + str(udp_header[1]) + "\n")
-        print("length:  " + str(udp_header[2]) + "\n")
-        if udp_header[0] == 'D':
-            print("payload:  " + str(data[0:4]) + "\n")
+        print(packet_type[udp_header[0]] + " Packet")
+        print("recv time   " + str(datetime.now()))
+        print("sender addr:  " + sender_ip + ":" + str(sender_port))
+        print("sequence:  " + str(udp_header[1]))
+        print("length:  " + str(udp_header[2]))
+        if udp_header[0] == b'D':
+            print("payload:  " + str(data[0:4]))
+            total_packets += 1
         else:
-            print("payload:  0\n")
-        total_packets += 1
+            print("payload:  0")
         total_bytes += udp_header[2]
 
         # leave space
-        print("\n")
+        print("")
 
         # end packet case (print summary info)
-        if udp_header[0] == 'E':
-            print("Summary\n")
-            print("Total Data packets  " + str(total_packets) + "\n")
-            print("Total Data bytes  " + str(total_bytes) + "\n")
+        if udp_header[0] == b'E':
+            print("Summary")
+            print("Total Data packets  " + str(total_packets))
+            print("Total Data bytes  " + str(total_bytes))
             end_time = time.time()
-            print("Average packets/second  " + str(total_packets/(end_time - start_time) + "\n")
-            print("Duration of the test  " + str(end_time - start_time) + "\n")
-            # reset timer
-            start_time = time.time()
+            print("Average packets/second  " + str(total_packets/(end_time - start_time)))
+            print("Duration of the test  " + str(end_time - start_time))
+            break
 
         # leave space
-        print("\n")
+        print("")
 
 
 def main():
@@ -83,23 +78,23 @@ def main():
 
     args = parser.parse_args()
 
-    tracker_lines = []
+    socket_obj.bind((this_ip_addr, int(args.p)))
 
+    tracker_lines = []
     tracker_file = open("tracker.txt", "r")
     curr_line = tracker_file.readline()
     while len(curr_line) != 0:
         send_parts = curr_line.split(" ")
-        send_parts[-1] = send_parts[-1].strip()
-        tracker_lines.append(send_parts)
+        if(send_parts[0] == args.o):
+            send_parts[-1] = send_parts[-1].strip()
+            tracker_lines.append(send_parts)
         curr_line = tracker_file.readline()
 
     tracker_lines.sort(key = lambda x: int(x[1]))
 
-    send_data(args.p, args.o, tracker_lines)
-
-    receive_data()
-
-
+    for part in tracker_lines:
+        send_data(part)
+        receive_data()
 
 
 if __name__ == "__main__":
--- sender.py
+++ sender.py
@@ -1,27 +1,22 @@
 import argparse
 import socket
 from datetime import datetime
+import struct
+import os
+import time
 
-def check_request(port, req_port, rate, curr_seq_no, length):
-    # create socket object
-    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    this_host = socket.gethostname()
-    this_ip_addr = socket.gethostbyname(this_host)
-    socket.bind((this_ip_addr, port))
+socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+this_host = socket.gethostname()
+this_ip_addr = socket.gethostbyname(this_host)
 
-    # second nulled out because ip is right but the port is wrong because the requester sends to a random port
-    full_packet, (requester_ip, _) = socket_obj.recvfrom(1024)
-    udp_header = full_packet[:9]
-    # requester fills payload with name of file its requesting
-    file_req = full_packet[9:]
-    udp_header = struct.unpack("!cII", udp_header)
-
+def process_request(file_req, requester_ip, req_port, rate, curr_seq_no, length):
     # maybe we end of file token
     remain_file_len = os.path.getsize(file_req)
     payload = ""
     extract_file = open(file_req, "r")
     while (remain_file_len > 0):
-    tell_length = 0
+        saveTime = time.time()
+        tell_length = 0
         # less bytes in file remaining than length
         if remain_file_len < length:
             payload = extract_file.read(remain_file_len)
@@ -33,50 +28,64 @@ def check_request(port, req_port, rate, curr_seq_no, length):
             remain_file_len -= length
 
         # create udp header (D since its a data packet)
-        udp_header = struct.pack("!cII", 'D', curr_seq_no, tell_length)
+        udp_header = struct.pack("!cII", b'D', curr_seq_no, tell_length)
 
         # print information
-        print("DATA Packet\n")
-        print("send time:  " + str(datetime.now()) + "\n")
-        print("requester ip address:  " + requester_ip + "\n")
-        print("requester port:  " + req_port + "\n")
-        print("Sequence num:  " + str(curr_seq_no) + "\n")
-        print("length:  " + str(tell_length) + "\n")
-        print("payload:  " + payload[0:4] + "\n")
+        print("DATA Packet")
+        print("send time:  " + str(datetime.now()))
+        print("requester addr:  " + requester_ip + ":" + str(req_port))
+        print("Sequence num:  " + str(curr_seq_no))
+        print("length:  " + str(tell_length))
+        print("payload:  " + payload[0:4])
         # extra space
-        print("\n")
+        print("")
 
         # increment sequence number
         curr_seq_no += tell_length
         # combine header with payload
-        packet_with_header = udp_header + payload
+        packet_with_header = udp_header + payload.encode()
 
         # send this packet to the requester
-        sender_addr = (requester_ip, req_port)
-        socket_obj.sendto(packet_with_header, sender_addr)
+        requester_addr = (requester_ip, req_port)
+        socket_obj.sendto(packet_with_header, requester_addr)
 
         # respect the rate
-        time.sleep(1/rate)
+        # time.sleep(1/rate)
+
+        while(time.time() - saveTime < 1/rate):
+            continue
 
     # send END packet once done
     # create udp header (E since it's an end packet)
-    udp_header = struct.pack("!cII", 'E', curr_seq_no, 0)
+    udp_header = struct.pack("!cII", b'E', curr_seq_no, 0)
     # combine header with payload (no payload for end packet)
-    packet_with_header = udp_header + ""
+    packet_with_header = udp_header
 
     # send this packet to the requester
-    sender_addr = (requester_ip, req_port)
-    socket_obj.sendto(packet_with_header, sender_addr)
-    print("END Packet\n")
-    print("send time:  " + str(datetime.now()) + "\n")
-    print("requester ip address:  " + requester_ip + "\n")
-    print("requester port:  " + req_port + "\n")
-    print("Sequence num:  " + str(curr_seq_no) + "\n")
-    print("length:  0\n")
-    print("payload:  \n")
+    requester_addr = (requester_ip, req_port)
+    socket_obj.sendto(packet_with_header, requester_addr)
+    print("END Packet")
+    print("send time:  " + str(datetime.now()))
+    print("requester addr:  " + requester_ip + ":" + str(req_port))
+    print("Sequence num:  " + str(curr_seq_no))
+    print("length:  0")
+    print("payload:  ")
     # extra space
-    print("\n")
+    print("")
 
+
+def check_request(req_port, rate, curr_seq_no, length):
+    while True:
+        # second nulled out because ip is right but the port is wrong because the requester sends to a random port
+        full_packet, (requester_ip, _) = socket_obj.recvfrom(1024)
+        udp_header = full_packet[:9]
+        # requester fills payload with name of file its requesting
+        file_req = full_packet[9:]
+        udp_header = struct.unpack("!cII", udp_header)
+        
+        if(udp_header[0] == b'R'):
+            process_request(file_req, requester_ip, req_port, rate, curr_seq_no, length)
+            break
 
 
 def main():
@@ -90,7 +99,9 @@ def main():
 
     args = parser.parse_args()
 
-    check_request(args.p, args.g, args.r, args.q, args.l)
+    socket_obj.bind((this_ip_addr, int(args.p)))
+
+    check_request(int(args.g), float(args.r), int(args.q), int(args.l))
 
 if __name__ == "__main__":
     main()

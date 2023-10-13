27d7e7b276b9accdfcb1c9bd1e8997e32bc509ad
Ben Wurster <wursterben17@gmail.com>
2023-10-13 16:25:52 -0500

    done

---
+++ runRequester.sh
@@ -0,0 +1,6 @@
+mkdir requester/
+cp requester.py requester/
+cp tracker2.txt requester/tracker.txt
+
+cd requester/
+python3 requester.py -p 5000 -o hello.txt
---
+++ runSenders.sh
@@ -0,0 +1,16 @@
+start=1
+end=$1
+
+touch tracker2.txt
+echo "" > tracker2.txt
+
+# Loop to create directories
+for ((i = start; i <= end; i++)); do
+  mkdir -p "sender$i/"
+  cp sender.py "sender$i/"
+  cp hello.txt "sender$i/hello.txt"
+  echo "hello.txt $i bwurster-XPS-15-7590 $((5000 + $i))" >> tracker2.txt
+  cd sender$i/
+  python3 sender.py -p "$((5000 + $i))" -g 5000 -r 1 -q 1000 -l 1 &
+  cd ..
+done
